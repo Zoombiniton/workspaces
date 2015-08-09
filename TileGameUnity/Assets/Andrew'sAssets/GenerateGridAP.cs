b50e1bd4bb10f14f5680024da6ab766bb80d8394
Zoombiniton <purplepacman@gmail.com>
2015-08-09 16:29:11 +0100

    TestsToGenerateGridAP

---
+++ TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateGridAP : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject square = new GameObject ("square", typeof(Plane));
+		//GameObject square = new Rect (100, 100, 100, 100);
+		square.transform.localScale = new Vector3(0.1f, 0.1f, 0.00f);
+		//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
+		square.transform.parent = GameObject.Find("GamePanel").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
